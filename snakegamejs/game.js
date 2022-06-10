caeaabca08db4ef1ec6f0344aba4961108d49ef6
Crypt1k1 <90605524+Crypt1k1@users.noreply.github.com>
2022-06-10 14:42:50 +0200

    Update game.js

--- snakegamejs/game.js
+++ snakegamejs/game.js
@@ -1,11 +1,12 @@
+//the import command is done correctly top1
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
-
+// good use of const and let commnads top2
 let lastRenderTime = 0
 let gameOver = false
 const gameBoard = document.getElementById('game-board')
-
+//not enough commnets tip 1
 function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press ok to start over.')) {
@@ -27,13 +28,13 @@ function main(currentTime) {
 }
 
 window.requestAnimationFrame(main)
-
+//always place comments of every function tip 3
 function update() {
   updateSnake()
   updateFood()
   checkDeath()
 }
-
+// good use of import commnads but in my opinion is too much tip 2
 function draw() {
   gameBoard.innerHTML = ''
   drawSnake(gameBoard)
@@ -43,3 +44,4 @@ function draw() {
 function checkDeath() {
   gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
+//code looks clean and nice top 3
